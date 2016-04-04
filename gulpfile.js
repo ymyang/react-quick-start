2f702869884c9bab73221eee0a4496aa62a54e0f
ymyang <ymyang@163.com>
2016-04-04 15:00:44 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ * Created by yang on 2016/4/4.
+ */
+'use strict';
+
+const fs = require('fs');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const webserver = require('gulp-webserver');
+
+const browserify = require('browserify');
+const babelify = require('babelify');
+const watchify = require('watchify');
+
+gulp.task('browserify', function() {
+    browserify('./index.jsx', {
+        debug: true,
+        plugin: [watchify]
+    })
+        .transform('babelify', { presets: ['es2015','react'] })
+        .bundle()
+        .on("error", function (err) {
+            console.log("Error : " + err.message);
+        })
+        .pipe(fs.createWriteStream('bundle.js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.jsx', ['browserify'])
+});
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            host: '127.0.0.1',
+            livereload: true
+        })
+    );
+});
+
+gulp.task('default', ['browserify', 'webserver']);
